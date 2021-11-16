596bdf2c051cf35dbb35d9af7b1f3f0562770baa
PhucVH <38422464+PhucVHGinar@users.noreply.github.com>
2021-11-16 19:39:28 +0700

    16-11-2021 07:39PM

---
+++ JavaScript/001. Conditions/004. Returning Boolean value from function.js
@@ -0,0 +1,18 @@
+function isLess(a, b) {
+    // Fix this code
+    if (a < b) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Change this value to test
+console.log(isLess(10, 5));
+console.log(isLess(1, 9));
+
+function myIsLess(a, b) {
+    return (a < b);
+}
+console.log(myIsLess(10, 15));
+console.log(myIsLess(4, 1));
---
+++ JavaScript/001. Conditions/006. Counting Cards.js
@@ -0,0 +1,30 @@
+var count = 0;
+
+function cc(card) {
+    switch(card) {
+        case 2:
+        case 3:
+        case 4:
+        case 5:
+        case 6:
+            count++; // count = count + 1
+            break;
+
+        case 10:
+        case "J":
+        case "Q":
+        case "K":
+        case "A":
+            count--; // count = count - 1
+            break;
+
+    }
+    var holdBet = 'Hold';
+    if (count > 0) {
+        holdBet = 'Bet'
+    }
+    return count + " " + "holdBet";
+}
+
+cc(2); cc('K'); cc(10); cc('K'); cc('A');
+console.log(cc(4));
