15c249b42f7ba683f3fce455470c109a2c382834
PhucVH <38422464+PhucVHGinar@users.noreply.github.com>
2021-11-16 20:00:55 +0700

    16-11-2021 08:00PM

---
+++ JavaScript/002. JavaScript Objects/001. Build JavaScript Objects.js
@@ -0,0 +1,17 @@
+// Example
+var ourDog = {
+    "name": "Campell", // name is property, campell is value 
+    "legs": 4, // legs is property, 4 is value
+    "tails": 1, // tails is property, 1 is value
+    "friends": ["everything!"]  /*friends is property, everthing is value => value can be number, string, array, 
+    or any data type in java script */
+};
+
+// Only change code below this line
+var myDog = {
+    "name": "Quincy",
+    "legs": 3,
+    "tails": 2,
+    "friends": []
+
+}
---
+++ JavaScript/002. JavaScript Objects/003. Accessing Object Properties with Bracket Notation.js
@@ -0,0 +1,15 @@
+// Setup
+
+var testObj = {
+    "an entree": "Hamburger",
+    "my side": "Veggies",
+    "the drink": "Water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];      // Change this line
+var drinkValue = testObj['the drink'];      // Change this line
+
+console.log(entreeValue);
+console.log(drinkValue);
